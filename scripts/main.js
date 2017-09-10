7797d22c8e4a657525b0b774af18e05ea17e4b1f
Bilal Hussain <bilal.hussain@incentro.com>
2017-09-10 23:03:10 +0200

    setup

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+function filter() {
+    var $filterButton = $('[data-filter]');
+    var $filterResetButton = $('[data-filter="all"]');
+    var $filterContentItems = $('[data-filter-content]');
+
+    // Filter button
+    $filterButton.on('click', function() {
+        var filterName = $(this).data('filter');
+        var $filterContent = $("[data-filter-content='" + filterName + "']");
+
+        $filterContent.siblings().addClass('inactive');
+        $filterContent.removeClass('inactive');
+    });
+
+    // Reset button
+    $filterResetButton.on('click', function() {
+        $filterContentItems.removeClass('inactive');
+    });
+
+    // Remember state
+    if (window.location.hash) {
+        $('[href="' + window.location.hash + '"]').click();
+    }
+}
+
+$(document).ready(function() {
+    filter();
+});
+
+
